9be18c4687dc12ab19521bf58f9d3b733f4732b6
AlexAdmer03 <alex.admer03@gmail.com>
2023-09-18 10:25:10 +0200

    update

--- ServiceApplikation/MVVM/Models/DeviceItem.cs
+++ ServiceApplikation/MVVM/Models/DeviceItem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +13,24 @@ namespace ServiceApplikation.MVVM.Models
         public string? Placement { get; set; }
         public string? DeviceType { get; set; }
         public bool IsActive { get; set; }
+        public string? Icon { get; set; }
+
+        private void SetIcon()
+        {
+            switch (DeviceType?.ToLower()) 
+            {
+                case "light":
+                    icon = "\uf0eb";
+                    break;
+                case "fan":
+                    icon = "\ue004b";
+                    break;
+                default:
+                    Icon = "\uf2db4";
+                    break;
+            
+            }
+
+        }
     }
 }
